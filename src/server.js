6eb2a4a9f699c187090185072a5146eb3cd04a7a
nig-srimaan <srimaankarra@gmail.com>
2026-06-16 20:37:36 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -17,8 +17,8 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true,
+  origin: '*',
+  credentials: false,
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
